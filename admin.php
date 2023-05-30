c14efed1abb12af1a048ef5ca113d923a141492a
Anisya Mahira Salienka <Anisya.Salienka@energibiz.com>
2023-05-30 20:56:25 +0700

    add logo

--- admin.php
+++ admin.php
@@ -7,13 +7,13 @@ require './connect.php';
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="author" content="Dimas Aldi Pratama">
-	<title>Sistem Pendukung keputusan pemilihan Supplier</title>
+	<title>Supplify</title>
 	<link rel="stylesheet" type="text/css" href="asset/css/style.css">
     <link rel="stylesheet" type="text/css" href="asset/plugin/font-icon/css/fontawesome-all.min.css">
 </head>
 <body>
 <header>
-    <img src="asset/image/logo.svg" id="logo-header">
+    <img src="asset/image/logo1.png" id="logo-header">
 </header>
 <nav>
     <?php include "nav.php"; ?>
